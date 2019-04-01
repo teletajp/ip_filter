967f8f5870b23007b79770c594f7957077ed7bbb
Александр <aleksandr@MBP-Aleksandr.Dlink>
2019-04-01 03:47:27 +0300

    Make ip_filter functional: filter by like, filter by any digits combinations, sort (no, asc, dsc). Add tests.

--- src/ip_filter.cpp
+++ src/ip_filter.cpp
@@ -1,17 +1,178 @@
 #include "ip_filter.h"
+#include <cctype>
+#include <iostream>
+
 namespace ip_tools
 {
-ip_filter::ip_filter(/* args */)
+
+ip4_addr_t::ip4_addr_t(const std::string &origin_ip)
+{
+    internal = "000.000.000.000";
+    int ii = 14;
+    int oi = (int)origin_ip.length() - 1;
+    int c = 0;//количество цифр в компоненте ip c>0 && c<4
+    int cc = 0;//количество компонентов ip cc==4
+    for(;ii>=0 && oi>=0;--ii)
+    {
+        char ch = origin_ip[oi];
+        if( std::isdigit(ch))
+        {
+            if(internal[ii] == '.')
+                break;
+            internal[ii] = ch;
+            ++c;
+            if(c>3)
+                break;
+        }
+        else if(ch == '.')
+        {
+            if(c == 0 || c > 3)
+                break;
+            if(internal[ii] != '.')
+                continue;
+            c = 0;
+            ++cc;
+        }
+        else
+            break;
+
+        --oi;
+    }
+    if(c)
+        ++cc;
+    if(cc == 4)
+    {
+        if(!( 
+            (internal[0] > '2' || (internal[0] == '2' && (internal[1] > '5' || (internal[1] == '5' && internal[2] > '5'))))
+            ||(internal[4] > '2' || (internal[4] == '2' && (internal[5] > '5' || (internal[5] == '5' && internal[6] > '5'))))
+            ||(internal[8] > '2' || (internal[8] == '2' && (internal[9] > '5' || (internal[9] == '5' && internal[10] > '5'))))
+            ||(internal[12] > '2' || (internal[12] == '2' && (internal[13] > '5' || (internal[13] == '5' && internal[14] > '5'))))
+        ))
+        origin = origin_ip;
+    }
+        
+}
+
+const ip4_addr_t& ip4_addr_t::operator=(const ip4_addr_t &o)
+{
+    origin = o.origin;
+    internal = o.internal;
+    return *this;
+}
+
+uint8_t ip4_addr_t::find(const std::string& like) const
+{
+    auto pos = internal.find(like);
+    if (pos == std::string::npos)
+        return 0;
+    if (pos < 4)
+        return 1;
+    if (pos < 8)
+        return 2;
+    if (pos < 12)
+        return 3;
+    return 4;
+}
+
+ip_filter_t::ip_filter_t():
+ip_list_size(0)
 {
 }
 
-ip_filter::~ip_filter()
+ip_filter_t::~ip_filter_t()
 {
 }
 
-uint32_t ip_filter::insert(const std::string &ip_string)
+const ip_filter_t &ip_filter_t::operator=(const ip_filter_t &oth)
 {
-    std::string str =ip_string;
-    return 0;
+    ip_list_size = oth.ip_list_size;
+    ip_list = oth.ip_list;
+    return *this;
 }
+
+bool ip_filter_t::insert(const ip4_addr_t& ip_addr)
+{
+    if(ip_addr.is_valid())
+    {
+        ip_list.push_back(ip_addr);
+        ++ip_list_size;
+        return true;
+    }
+    return false;
+}
+
+bool ip_filter_t::insert(const std::string &ip_string)
+{
+    ip4_addr_t ip(ip_string);
+    if(ip.is_valid())
+    {
+        ip_list.push_back(ip);
+        ++ip_list_size;
+        return true;
+    }
+    return false;
+}
+
+std::tuple<uint32_t,uint32_t> ip_filter_t::list_insert(const std::list<std::string> &ip_string_list)
+{
+    uint32_t inserted = 0;
+    uint32_t not_inserted = 0;
+    for (const auto &ip_string : ip_string_list)
+    {
+        if (insert(ip_string))
+            ++inserted;
+        else
+            ++not_inserted;
+    }
+    return std::make_tuple(inserted, not_inserted);
+}
+
+void ip_filter_t::print()
+{
+    for (const auto &ip : ip_list)
+    {
+        std::cout << ip.get_origin() << std::endl;
+    }
+}
+
+void ip_filter_t::sort(order_t order)
+{
+    if (order == asc_order)
+        ip_list.sort();
+    else
+        ip_list.sort(ip4_addr_t::less_comp);
+}
+
+ip_filter_ptr ip_filter_t::select(const std::string& like, order_t order)
+{
+    auto res = std::make_shared<ip_filter_t>();
+    for(const auto& ip:ip_list)
+    {
+        if(ip.find(like))
+        res->insert(ip);
+    }
+    res->sort(order);
+    return res;
+}
+
+ip_filter_ptr ip_filter_t::select( const std::string& n1, const std::string& n2, const std::string& n3, const std::string& n4, order_t order)
+{
+    auto res = std::make_shared<ip_filter_t>();
+    for (const auto &ip : ip_list)
+    {
+        if (!n1.empty() && ip.find(n1) != 1)
+            continue;
+        if (!n2.empty() && ip.find(n2) != 2)
+            continue;
+        if (!n3.empty() && ip.find(n3) != 3)
+            continue;
+        if (!n4.empty() && ip.find(n4) !=41)
+            continue;
+
+        res->insert(ip);
+    }
+    res->sort(order);
+    return res;
+}
+
 }
--- src/ip_filter.h
+++ src/ip_filter.h
@@ -1,14 +1,57 @@
 #include <stdint.h>
 #include <string>
+#include <list>
+#include <tuple>
+#include <memory>
 namespace ip_tools
 {
-class ip_filter
+
+class ip4_addr_t
 {
-private:
-    /* data */
+    std::string internal;
+    std::string origin;
 public:
-    ip_filter(/* args */);
-    ~ip_filter();
-    uint32_t insert(const std::string &ip_string);
+    static bool less_comp(const ip4_addr_t &l,const ip4_addr_t &r) {return l.internal>r.internal;};
+    ip4_addr_t(){};
+    ip4_addr_t(const std::string &origin_ip);
+    ip4_addr_t(const ip4_addr_t &other):internal(other.internal), origin(other.origin){};
+    const ip4_addr_t& operator=(const ip4_addr_t &o);
+    ~ip4_addr_t(){};
+    inline bool is_valid() const {return !origin.empty();};
+    const std::string& get_origin() const {return origin;};
+    inline bool operator <(const ip4_addr_t &oth) const{return internal<oth.internal;};
+    uint8_t find(const std::string& like) const;
 };
-}
+
+typedef std::list<ip4_addr_t> ip4_list_t;
+typedef std::shared_ptr<ip4_list_t> ip4_list_ptr;
+
+enum order_t
+{
+    no_order = 0,
+    asc_order,
+    dsc_order,
+    max_value = dsc_order
+};
+
+class ip_filter_t
+{
+    uint32_t ip_list_size;
+    ip4_list_t ip_list;
+    bool insert(const ip4_addr_t& ip_addr);
+  public:
+    ip_filter_t();
+    ~ip_filter_t();
+    ip_filter_t(const ip_filter_t &oth):ip_list_size(oth.ip_list_size), ip_list(oth.ip_list){};
+    const ip_filter_t &operator=(const ip_filter_t &oth);
+    inline uint32_t count() const {return ip_list_size;};
+    bool insert(const std::string &ip_string);
+    std::tuple<uint32_t/*inserted*/,uint32_t/*not_inserted*/> list_insert(const std::list<std::string> &ip_string_list);
+    std::shared_ptr<ip_filter_t> select( const std::string& n1, const std::string& n2, const std::string& n3, const std::string& n4, order_t order);
+    std::shared_ptr<ip_filter_t> select( const std::string& like,order_t order);
+    void sort(order_t order);
+    void print();
+};
+
+typedef std::shared_ptr<ip_filter_t> ip_filter_ptr;
+} // namespace ip_tools
